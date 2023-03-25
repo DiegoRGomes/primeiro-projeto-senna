f6f849951cd109cfdfa9ddccafb329724f1d277b
Diego Rocha Gomes <diego.goomes@outlook.com>
2023-03-25 11:59:02 -0300

    Add files via upload

---
+++ SRC/JS/index.js
@@ -0,0 +1,30 @@
+
+
+
+console.log(document.querySelectorAll('.botao'));
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+       
+        const botaoSelecionado = document.querySelector('.selecionado')
+        botaoSelecionado.classList.remove('selecionado');
+
+   
+        botao.classList.add('selecionado');
+        
+        
+        const imagemAtiva = document.querySelector('.ativa');
+        imagemAtiva.classList.remove('ativa');
+
+       
+        imagens[indice].classList.add('ativa');
+
+    })
+})            
+  
